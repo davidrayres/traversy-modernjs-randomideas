2c6eef581eea302a8dd0d2098f71eab9ed919177
David Ayres <davidrayres@hotmail.com>
2023-03-03 09:09:38 -0500

    Database Queries

--- routes/ideas.js
+++ routes/ideas.js
@@ -1,19 +1,77 @@
 import express from 'express'
+import Idea from '../models/idea.js'
 const router = express.Router()
 
-const ideas = [{id: 1}, {id: 2}, {id: 3}, {id: 4}]
-
-//get all ideas
-router.get('/', (req, res) => {
-  return res.send({sucess: true, data: ideas})
+// Get all ideas
+router.get('/', async (req, res) => {
+  try {
+    const ideas = await Idea.find()
+    res.json({success: true, data: ideas})
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({success: false, error: 'Something went wrong.  Try again later.'})
+  }
 })
 
-//get single idea
-router.get('/:id', (req, res) => {
-  const idea = ideas.find(idea => idea.id === +req.params.id)
-  if (!idea) return res.status(404).send({success: false, error: 'Resource not found.'})
+// Get single idea
+router.get('/:id', async (req, res) => {
+  try {
+    const idea = await Idea.findById(req.params.id)
+    if (!idea) return res.status(404).json({success: false, error: 'Resource not found'})
+    res.send({sucess: true, data: idea})
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({success: false, error: 'Something went wrong.  Try again later.'})
+  }
 
-  res.send({sucess: true, data: idea})
+  res.json({success: true, data: idea})
+})
+
+// Add an idea
+router.post('/', async (req, res) => {
+  const idea = new Idea({
+    text: req.body.text,
+    tag: req.body.tag,
+    username: req.body.username,
+  })
+
+  try {
+    const savedIdea = await idea.save()
+    res.json({success: true, data: savedIdea})
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({success: false, error: 'Something went wrong.  Try again later.'})
+  }
+})
+
+// Update idea
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedIdea = await Idea.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          text: req.body.text,
+          tag: req.body.tag,
+        },
+      },
+      {new: true}
+    )
+    res.send({success: true, data: updatedIdea})
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({success: false, error: 'Something went wrong.  Try again later.'})
+  }
+})
+
+// Delete idea
+router.delete('/:id', async (req, res) => {
+  try {
+    await Idea.findByIdAndDelete(req.params.id)
+    res.send({success: true, data: {}})
+  } catch (error) {
+    res.status(500).json({success: false, error: 'Something went wrong.  Try again later.'})
+  }
 })
 
 export default router
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 import express from 'express'
+import dotenv from 'dotenv'
 import ideasRouter from './routes/ideas.js'
+import connectDB from './config/db.js'
+
 const app = express()
-const PORT = 5000
-0
+dotenv.config()
+connectDB()
+
+app.use(express.json()) //allows express to parse json data
+app.use(express.urlencoded({extended: false})) //allows express to parse form data
+app.use('/api/ideas', ideasRouter)
+
 app.get('/', (req, res) => {
   return res.send({message: 'Welcome to App'})
 })
 
-app.use('/api/ideas', ideasRouter)
-
+const PORT = process.env.PORT
 const ideaRouter = app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`))
